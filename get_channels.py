576f2783a00c7b24d83a147113564f13b673025f
caridamarketing <msavenko@carida.com>
2023-12-30 12:57:13 -0500

    first

---
+++ get_channels.py
@@ -0,0 +1,33 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import logging
+
+def get_channels_from_google_doc():
+    try:
+        # Replace 'YOUR_GOOGLE_DOC_CREDENTIALS.json' with the path to your Google Doc credentials file
+        CREDENTIALS = ServiceAccountCredentials.from_json_keyfile_name('config/grand-sphere-343001-881f612add4a.json')
+
+        # Replace 'YOUR_GOOGLE_DOC_ID' with the ID of your Google Doc
+        GOOGLE_DOC_ID = '1PF7EZJs4G8X46zI5W5NKzozdPEbvNmrKyydb2VuUmgU'
+
+        # Authenticate with Google Sheets API
+        client = gspread.authorize(CREDENTIALS)
+
+        # Open the Google Doc
+        google_doc = client.open_by_key(GOOGLE_DOC_ID)
+
+        # Get the first sheet of the Google Doc
+        sheet = google_doc.get_worksheet(0)
+
+        # Get all values from the B column second row till the end of the sheet
+        list_of_strings = sheet.col_values(2)[1:]
+
+        return list_of_strings
+    except Exception as e:
+        logging.exception("An exception with Google Dock occurred")
+
+result = get_channels_from_google_doc()
+
+print(result)
+
+
---
+++ main.py
@@ -0,0 +1,125 @@
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetHistoryRequest
+from telethon.tl.types import PeerChannel
+from telethon.errors import SessionPasswordNeededError
+from telethon.sessions import MemorySession
+from telethon.sessions import StringSession
+
+from get_channels import get_channels_from_google_doc
+from config import auth
+
+import datetime
+from datetime import datetime, timezone
+import csv
+from telethon.tl.functions.channels import GetFullChannelRequest
+import os
+
+
+
+class TelegramNews:
+    def __init__(self, api_id, api_hash, bot_token, phone, password, session_id, CHANNELS):
+        self.api_id = api_id
+        self.api_hash = api_hash
+        self.bot_token = bot_token
+        self.phone = phone
+        self.password = password
+        self.session_id = session_id
+        self.channels = CHANNELS
+        self.client = None
+
+    def authenticate(self):
+        """
+        Authenticates the user with the Telegram API.
+
+        This method connects to the Telegram API using the provided API ID and API hash.
+        If the user is not already authorized, it sends a code request to the user's phone number
+        and prompts the user to enter the received code for authentication.
+
+        If the provided session ID is invalid, it falls back to using a memory session for authentication.
+
+        Raises:
+            SessionPasswordNeededError: If the user account has a password set and requires password authentication.
+        """
+        self.client = TelegramClient(MemorySession(), self.api_id, self.api_hash)
+        self.client.connect()
+        if not self.client.is_user_authorized():
+            try:
+                # Try String Session Authentication
+                session_string = self.session_id
+                self.client = TelegramClient(StringSession(session_string), self.api_id, self.api_hash)
+                self.client.connect()
+                if not self.client.is_user_authorized():
+                    self.client.send_code_request(self.phone)
+                    try:
+                        self.client.sign_in(self.phone, input('Enter the code: '))
+                    except SessionPasswordNeededError:
+                        self.client.sign_in(password=self.password)
+            except ValueError:
+                # Fall back to Memory Session Authentication
+                self.client = TelegramClient(MemorySession(), self.api_id, self.api_hash)
+                self.client.connect()
+                if not self.client.is_user_authorized():
+                    self.client.send_code_request(self.phone)
+                    try:
+                        self.client.sign_in(self.phone, input('Enter the code: '))
+                    except SessionPasswordNeededError:
+                        self.client.sign_in(password=self.password)
+
+    def get_messages(self):
+        """
+        Retrieves messages from specified channels within a given date range and writes them to a CSV file.
+        
+        Returns:
+            None
+        """
+        start_date = datetime(2023, 12, 24, tzinfo=timezone.utc)
+        end_date = datetime(2023, 12, 31, tzinfo=timezone.utc)
+
+        with open('messages.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            # Check if the file is empty
+            if os.stat('messages.csv').st_size == 0:
+                writer.writerow(['Date', 'ID', 'Creation Date', 'Number of Subscribers', 'Channel Name', 'Views', 'Message'])
+
+            for channel in CHANNELS:
+                channel_entity = self.client.get_entity(channel)
+                full_channel = self.client(GetFullChannelRequest(channel=channel_entity))
+                posts = self.client(GetHistoryRequest(
+                    peer=PeerChannel(channel_entity.id),
+                    limit=3,
+                    offset_date=None,
+                    offset_id=0,
+                    max_id=0,
+                    min_id=0,
+                    add_offset=0,
+                    hash=0))
+                
+                
+                channel_name = channel_entity.title
+                channel_creation_date = channel_entity.date
+                channel_subscribers_count = full_channel.full_chat.participants_count
+            
+                for message in posts.messages:
+                    if start_date <= message.date <= end_date:
+                        writer.writerow([message.date, message.id, channel_creation_date, channel_subscribers_count, channel_name, message.views, message.message])
+
+    def run(self):
+        self.authenticate()
+        self.get_messages()
+        self.client.disconnect()
+
+
+API_ID = auth.API_ID
+API_HASH = auth.API_HASH
+BOT_TOKEN = auth.BOT_TOKEN
+PHONE = auth.PHONE  # in international format
+PASSWORD = auth.PASSWORD  # if your account has two-step verification enabled
+SESSION_ID = auth.SESSION_ID
+CHANNELS = get_channels_from_google_doc()
+
+telegram_news = TelegramNews(API_ID, API_HASH, BOT_TOKEN, PHONE, PASSWORD, SESSION_ID, CHANNELS)
+
+if __name__ == '__main__':
+
+    telegram_news.run()
+
